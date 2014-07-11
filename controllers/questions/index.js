dd56ad9c6b58ff62b5964ea9209e8dccb728c739
isaacd9 <isaacd9@gmail.com>
2014-07-11 05:23:30 +0000

    [Master] Boilerplate controller code. View alsmot working

--- controllers/questions/index.js
+++ controllers/questions/index.js
@@ -1,3 +1,33 @@
+var models = require('../../models');
+var question = models.question;
+
 module.exports.show = function(req, res, next) {
-	res.render('index', {what: 'nothing'});
+	getQuestion = function(id, next) {
+		next(id, 0, 0, "mock");
+	}
+
+	renderQuestion = function(id, yes, no, question) {
+		res.render('viewquestion', {id: id, yes: yes, no: no, question: question});
+	}
+
+	if(!!req.params.id) {
+		console.log(req.params.id);
+		getQuestion(req.params.id, renderQuestion);		
+	}
+	else {
+		question.count().success(function(totalQuestions) {
+			id = Math.floor((Math.random()*totalQuestions) + 1);
+			getQuestion(id, renderQuestion);
+		})
+	}
 };
+
+module.exports.submit = function(req, res, next) {
+	res.render('submit');
+}
+
+module.exports.handleSubmit = function(req, res, next) {
+	console.log(req.body.question);
+	id=0;
+	res.redirect('/view/'+id);
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser');
 
 var controllers = require('./controllers');
 
@@ -10,9 +11,14 @@ app.set('view engine', 'html.mst');
 app.set('views', __dirname + '/views');
 
 app.use("/public", express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.route('/').get(controllers.questions.show);		
+app.route('/view/:id').get(controllers.questions.show);
 
+app.route('/submit').get(controllers.questions.submit);
+app.route('/submit').post(controllers.questions.handleSubmit);
 
 var port = 8000;
 app.listen(port);
